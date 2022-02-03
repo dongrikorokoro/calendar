0274a946de2253983524b716c09d78269d7826d9
dongrikorokoro <91802434+dongrikorokoro@users.noreply.github.com>
2022-02-04 06:11:19 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+      },
+      initialDate: '2022-02-01',
+      navLinks: true, 
+      selectable: true,
+      selectMirror: true,
+      select: function(arg) {
+        var title = prompt('タイトル:');
+        if (title) {
+          calendar.addEvent({
+            title: title,
+            start: arg.start,
+            end: arg.end,
+            allDay: arg.allDay
+          })
+          localStorage.setItem('events',events)
+        }
+        calendar.unselect()
+      },
+      eventClick: function(arg) {
+        if (confirm('本当に削除してもいいですか？')) {
+          arg.event.remove()
+        }
+      },
+      editable: true,
+      dayMaxEvents: true, 
+      events: []
+    });
+    events = localStorage.getItem('events')
+    calendar.render();
+  });
